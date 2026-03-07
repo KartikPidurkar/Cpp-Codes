b22e39b83fcb40a57e8454b021e571568c40b837
Kartik Pidurkar <kartikpidurkar@gmail.com>
2026-03-07 17:45:58 +0530

    Add files via upload

---
+++ 14_Ternary_Op.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int grade = 75;
+    grade >=60 ? cout<<"You pass" : cout<<"you fail";
+    return 0;
+}
+
+// Ternary operator expression 
+
+// condition ? true(pass statement) : false(failed statement) ;
