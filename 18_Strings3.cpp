1885e682a032e35d488acf5af023624aff65265d
Kartik Pidurkar <kartikpidurkar@gmail.com>
2026-03-08 20:42:47 +0530

    Add files via upload

---
+++ 18_Strings3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    string name;
+    cout<<"Enter the name :"<<endl;
+    getline(cin,name);
+
+    //name.clear(); // clears the input from user
+    //name.append("@gmail.com"); // adds this to the string 
+    //cout<<name.at(0);         // Specific index position value
+    
+    //cout<<name<<endl;
+    return 0;
+}
