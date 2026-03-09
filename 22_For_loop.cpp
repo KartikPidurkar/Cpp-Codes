b44f2e03c968a6e4045230d5a54e4d41e9ebdfe9
Kartik Pidurkar <kartikpidurkar@gmail.com>
2026-03-09 17:06:24 +0530

    Add files via upload

---
+++ 22_For_loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    //for(int index = 1;index<10;index++)
+    for(int index = 10;index>=0;index-=2)
+    {
+        cout<<index<<endl;
+        cout<<"Hello Evernyan"<<endl;
+    }
+
+
+
+    
+    return 0;
+}
